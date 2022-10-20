1f624d464e628e9e5ffae9cfcec879c397f924e2
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2022-10-19 20:03:25 -0600

    Se comentan los metodos de la lista y la cola

--- src/Cola.cpp
+++ src/Cola.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 Cola::Cola(){
+// Se inicializan los atributos de la clase
     this->inicio=0;
     this->final=-1;
     this->capacidad=10;
@@ -12,6 +13,7 @@ Cola::Cola(){
     this->contador=0;
 }
 Cola::~Cola(){
+// Se dejan en cero todos los atributos de la clase y se libera la memoria utilizada
     this->inicio=0;
     this->final=0;
     this->capacidad=10;
@@ -19,9 +21,12 @@ Cola::~Cola(){
     delete this->cola;
 }
 void Cola::Encolar(int elemento){
+// Se verifica si la cola tiene espacio, de no ser asi no se puede encolar
     if(this->contador==this->capacidad){
         cout << "La cola esta llena"<<endl;
     }else{
+// En caso de tener espacio se modifica el final y se incluye el nuevo 
+// valor
         this->final=(this->final+1)%this->capacidad;
         this->cola[this->final]=elemento;
         ++this->contador;
@@ -29,10 +34,12 @@ void Cola::Encolar(int elemento){
 
 }
 int Cola::Desencolar(){
+// Se verifica si la cola esta vacia, de ser asi no se puede desencolar de ella
     if(this->contador==0){
         cout << "La cola esta vacía" << endl;
         return 123;
     }else{
+// Si no lo está, se modifica el inicio y se devuelve el primer valor agregado
         int elemDevolver=cola[this->inicio];
         this->inicio=(this->inicio+1)%this->capacidad;
         --this->contador;
@@ -41,5 +48,6 @@ int Cola::Desencolar(){
     
 }
 int Cola::numElem(){
+// Se devuelve la cantidad de elementos en la cola
     return this->contador;
 }
--- src/listaIndexada.cpp
+++ src/listaIndexada.cpp
@@ -5,21 +5,28 @@
 using namespace std;
  
 ListaIndexada::ListaIndexada(){
+// Se inicializan los atributos de la clase
     this->primero=nullptr;
     this->ultimo=nullptr;
     this->contador=0;
 }
 ListaIndexada::~ListaIndexada(){
+// Se libera la memoria utilizada
     delete this->primero;
 }
 void ListaIndexada::borrar(int indice){
+// Se crea un puntero para iterar por la lista
     Caja* actual=this->primero;
+// Se verifica si el borrado se hace al frente de la lista
     if(indice==1){
+// En caso de serlo se borra la primera posicion y
+// El puntero al primer elemento apunta al antiguo segundo
         Caja* cajaBorrar=this->primero;
         this->primero=this->primero->siguiente;
         cajaBorrar->siguiente=nullptr;
         delete cajaBorrar;
     }else{
+// De no serlo se elimina el elemento normalmente
         for (int i=1;i<indice-1;++i){
         actual=actual->siguiente;
         }
@@ -28,40 +35,60 @@ void ListaIndexada::borrar(int indice){
         cajaBorrar->siguiente=nullptr;
         delete cajaBorrar;
     }
+// Se verifica si ha cambiado el ultimo 
+// En caso de haberlo hecho se modifica el puntero al ultimo
     if(this->contador==indice){
         this->ultimo=actual;
     }
+// Se decrementa la cantidad de elementos de la lista
     --contador;
 }
+
 void ListaIndexada::insertar(int dato,int indice){
+// Se crea una nueva caja con el numero por insertar
     Caja* caja=new Caja(dato);
+// Se crea un puntero para iterar por la lista
     Caja* actual=this->primero;
+// Se verifica si el insertado se hace al frente de la lista
+// En caso de serlo se inserta en la primera posicion y
+// El puntero al primer elemento apunta a la nueva caja
     if(indice==1){
         caja->siguiente=this->primero;
         this->primero=caja;
     }
+// En cualquier otro caso se inserta normalmente,recorriendo la lista
+// menos si la insercion es al final 
     else if(indice==this->contador && indice!=1){
         this->ultimo->siguiente=caja;
     }else{
+// Cuando la insercion es al final se usa el puntero al ultimo 
+// y se inserta directamente al final de la lista
         for (int i=1;i<indice-1;++i){
         actual=actual->siguiente;
         }
         caja->siguiente=actual->siguiente;
         actual->siguiente=caja;
     }
+// Se aumenta la cantidad de elementos 
     ++this->contador;
+// Se verifica si ha cambiado el ultimo 
+// En caso de haberlo hecho se modifica el puntero al ultimo
     if(this->contador==indice){
         this->ultimo=caja;
     }
 }
 int ListaIndexada::recuperar(int indice){
+// Se itera por la lista para llegar al indice deseado
     Caja* actual=this->primero;
     for (int i=1;i<indice;++i){
     actual=actual->siguiente;
     }
+// El dato en ese indice se devuelve
     return actual->dato;
 }
 void ListaIndexada::modificar(int indice,int dato){
+// Se itera por la lista para llegar al indice deseado
+// y se modifica con el dato ingresado 
     Caja* actual=this->primero;
     for (int i=1;i<indice;++i){
     actual=actual->siguiente;
@@ -69,13 +96,17 @@ void ListaIndexada::modificar(int indice,int dato){
     actual->dato=dato;
 }
 int ListaIndexada::numElem(){
+// Se devuelve la cantidad de datos en la lista
     return this->contador;
 }
 ListaIndexada::Caja::Caja(int dato){
+// Se inicializan los atributos de la caja
     this->dato=dato;
     this->siguiente=nullptr;
 }
 ListaIndexada::Caja::~Caja(){
+// En caso de haber,
+// Se manda a liberar la memoria del siguiente elemento de la caja 
     if(this->siguiente!=nullptr){
         delete this->siguiente;
     }
