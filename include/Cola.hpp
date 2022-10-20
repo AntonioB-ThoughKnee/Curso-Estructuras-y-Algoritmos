9bf56c8fb41f88be1b45cb0c9bfdb32cb83a1d3d
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2022-10-19 19:44:15 -0600

    Documentacion de las clases Cola y ListaIndexada

--- include/Cola.hpp
+++ include/Cola.hpp
@@ -1,5 +1,15 @@
 #ifndef COLAHPP
 #define COLAHPP
+/**
+ * @brief Estructura de datos Cola
+ * @remark Esta estructura es implementada por un arreglo circular
+ * @remark Específicamente esta cola admite un limite de 10 elementos
+ * @param inicio variable que guarda la posicion del primer dato de la cola
+ * @param final variable que guarda la posicion del ultimo dato de la cola
+ * @param capacidad cantidad maxima de datos en la cola
+ * @param cola puntero al arreglo circular
+ * @param contador cantidad de datos en la cola
+ */
 class Cola{
 
 private:
@@ -10,10 +20,47 @@ int* cola;
 int contador;
 
 public:
+/**
+ * @brief Metodo constructor de la clase
+ * @remark Efecto: Inicializa la cola como una cola vacía.
+ * @remark Requiere: Que la cola no esté inicializada.
+ * @remark Modifica: La cola.
+ */
 Cola();
+/**
+ * @brief Metodo destructor de la clase
+ * @remark Efecto: Hace que la cola quede inutilizable.
+ * @remark Requiere: Que la cola no este destruida.
+ * @remark Modifica: La cola.
+ * 
+ */
 ~Cola();
+/**
+ * @brief Metodo para agregar un elemento a la cola
+ * 
+ * @param elemento elemento que se desea incluir a la cola
+ * @remark Efecto: Agrega un elemento en la parte de atrás de cola.
+ * @remark Requiere: Que la cola esté inicializada y que no se haya excedido la capacidad maxima de la misma.
+ * @remark Modifica: El contenido de la cola.
+ */
 void Encolar(int elemento);
+/**
+ * @brief Metodo para agregar un elemento a la cola
+ * 
+ * @return int elemento que se saco de la cola
+ * @remark Efecto: Elimina el elemento que se encuentra delante de la cola y lo devuelve.
+ * @remark Requiere: Que la cola esté inicializada y no vacía.
+ * @remark Modifica: El contenido de la cola.
+ */
 int Desencolar();
+/**
+ * @brief Metodo que devuelve la cantidad de elementos en la cola
+ * 
+ * @return int cantidad de elementos en la cola 
+ * @remark Efecto: Devuelve un entero con el número de elementos presente en la cola
+ * @remark Requiere: Que la cola esté inicializada.
+ * @remark Modifica: No produce modificaciones en la estructura.
+ */
 int numElem();
 };
 #endif
--- include/ListaIndexada.hpp
+++ include/ListaIndexada.hpp
@@ -1,33 +1,111 @@
 #ifndef LISTAINDEXADAHPP
 #define LISTAINDEXADAHPP
-
+/**
+ * @brief Estructura de datos ListaIndexada
+ * @remark Esta estructura es implementada por una lista simplemente enlazada
+ * @remark La estructura cuenta con un puntero al ultimo y un contador de elementos
+ * @param primero puntero al primer elemento de la lista 
+ * @param ultimo puntero al ultimo elemento de la lista
+ * @param contador cantidad de elementos en la lista
+ * 
+ */
 class ListaIndexada
 {
-
-class Nodo
+/**
+ * @brief Clase que define el grupo de datos guardado en cada indice de la lista
+ * @param dato numero guardado en esa caja de la lista 
+ * @param siguiente puntero a la proxima caja de la lista
+ * 
+ */
+class Caja
 {
     friend class ListaIndexada;
     private:
     int dato;
-    Nodo* siguiente;
+    Caja* siguiente;
     public:
-    Nodo(int dato);
-    ~Nodo();
+    /**
+     * @brief Constructor de la clase caja
+     * 
+     * @param dato numero guardado en esa caja de la lista 
+     */
+    Caja(int dato);
+    /**
+     * @brief Destructor de la clase caja
+     * 
+     */
+    ~Caja();
 };
 
 private:
-
-    Nodo* primero;
-    Nodo* ultimo;
+    Caja* primero;
+    Caja* ultimo;
     int contador;
 public:
+    /**
+     * @brief Constructor de la lista
+     * 
+     * @remark Efecto: Crea una lista de tipo Lista Indexada y la guarda en el parámetro dado.
+     * @remark Requiere: Objeto no inicializado de tipo Lista Indexada.
+     * @remark Modifica: La lista.
+     */
     ListaIndexada();
+    /**
+     * @brief Destructor de la lista
+     * @remark Efecto: Destruye una lista de tipo lista_indexada.
+     * @remark Requiere: Que la lista indexada ya haya sido creada anteriormente.
+     * @remark Modifica: La lista indexada, ya que el operador básico se dedica a destruirla.
+     * 
+     */
     ~ListaIndexada();
+    /**
+     * @brief Metodo que inserta un elemento en una posicion de la lista
+     * 
+     * @param dato numero por guardar en la lista
+     * @param posicion indice donde se desea insertar el numero
+     * @remark Efecto: Inserta un nuevo elemento (enviado por parámetro)
+     * en la posición especificada por parámetro.
+     * @remark Requiere: Índice sea válido.(Por ejemplo: si la lista tiene 5 elementos, el indice 7 no es valido)
+     * @remark Modifica: Se modifica a la lista indexada L.
+     */
     void insertar(int dato,int posicion);
+    /**
+     * @brief Metodo que borra un elemento de una posicion de la lista
+     * 
+     * @param posicion indice del elemento que se desea borrar
+     * @remark Efecto: Elimina un elemento de una posición específica de la lista indexada.
+     * @remark Requiere: Índice sea válido para la lista.
+     * @remark Modifica: Modifica a la lista L, ya que el operador básico se dedica a borrar un elemento entonces el estado de la lista indexada cambia.
+     */
     void borrar(int posicion);
+    /**
+     * @brief Metodo que devuelve el numero de un indice en especifico
+     * 
+     * @param posicion ndice del elemento que se desea recuperar
+     * @return int numero recuperado
+     * @remark Efecto: Retorna el elemento que se encuentra en la posición i, pasada por parámetro.
+     * @remark Requiere: índice válido dentro del rango de la lista.
+     * @remark Modifica: Este operador básico no realiza modificaciones sobre la lista indexada.
+     */
     int recuperar(int posicion);
+    /**
+     * @brief Metodo que modifica un dato de un indice en la lista
+     * 
+     * @param dato nuevo elemento por poner en el indice 
+     * @param posicion indice del elemento por modificar
+     * @remark Efecto: Reemplaza el elemento que se encuentra en el índice i, por el elemento que se pasa por parámetro llamado e, en la lista indexada.
+     * @remark Requiere: i= una posición válida.
+     * @remark Modifica: L, ya que hace un reemplazo de un elemento.
+     */
     void modificar(int dato,int posicion);
+    /**
+     * @brief Metodo que devuelve la cantidad de elementos de la lista
+     * 
+     * @remark Efecto: Cuenta la cantidad de elementos que contiene la lista indexada L.
+     * @remark Requiere: L = lista indexada inicializada
+     * @remark Modifica: Este operador básico no realiza modificaciones.
+     * 
+     */
     int numElem();
-    void imprimir();
 };
 #endif
