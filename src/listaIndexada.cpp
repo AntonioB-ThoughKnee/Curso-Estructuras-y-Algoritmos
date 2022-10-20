f22763f46eeeda9557bab25d7504e3d61a5f304c
Alejandro Jimenez Corea <pichicareto24@gmail.com>
2022-10-19 19:45:22 -0600

    Cambios en lista indexada

--- src/listaIndexada.cpp
+++ src/listaIndexada.cpp
@@ -13,20 +13,20 @@ ListaIndexada::~ListaIndexada(){
     delete this->primero;
 }
 void ListaIndexada::borrar(int posicion){
-    Nodo* actual=this->primero;
+    Caja* actual=this->primero;
     if(posicion==1){
-        Nodo* nodoBorrar=this->primero;
+        Caja* cajaBorrar=this->primero;
         this->primero=this->primero->siguiente;
-        nodoBorrar->siguiente=nullptr;
-        delete nodoBorrar;
+        cajaBorrar->siguiente=nullptr;
+        delete cajaBorrar;
     }else{
         for (int i=1;i<posicion-1;++i){
         actual=actual->siguiente;
         }
-        Nodo* nodoBorrar=actual->siguiente;
+        Caja* cajaBorrar=actual->siguiente;
         actual->siguiente=actual->siguiente->siguiente;
-        nodoBorrar->siguiente=nullptr;
-        delete nodoBorrar;
+        cajaBorrar->siguiente=nullptr;
+        delete cajaBorrar;
     }
     if(this->contador==posicion){
         this->ultimo=actual;
@@ -34,35 +34,35 @@ void ListaIndexada::borrar(int posicion){
     --contador;
 }
 void ListaIndexada::insertar(int dato,int posicion){
-    Nodo* nodo=new Nodo(dato);
-    Nodo* actual=this->primero;
+    Caja* caja=new Caja(dato);
+    Caja* actual=this->primero;
     if(posicion==1){
-        nodo->siguiente=this->primero;
-        this->primero=nodo;
+        caja->siguiente=this->primero;
+        this->primero=caja;
     }
     else if(posicion==this->contador && posicion!=1){
-        this->ultimo->siguiente=nodo;
+        this->ultimo->siguiente=caja;
     }else{
         for (int i=1;i<posicion-1;++i){
         actual=actual->siguiente;
         }
-        nodo->siguiente=actual->siguiente;
-        actual->siguiente=nodo;
+        caja->siguiente=actual->siguiente;
+        actual->siguiente=caja;
     }
     ++this->contador;
     if(this->contador==posicion){
-        this->ultimo=nodo;
+        this->ultimo=caja;
     }
 }
 int ListaIndexada::recuperar(int posicion){
-    Nodo* actual=this->primero;
+    Caja* actual=this->primero;
     for (int i=1;i<posicion;++i){
     actual=actual->siguiente;
     }
     return actual->dato;
 }
 void ListaIndexada::modificar(int posicion,int dato){
-    Nodo* actual=this->primero;
+    Caja* actual=this->primero;
     for (int i=1;i<posicion;++i){
     actual=actual->siguiente;
     }
@@ -71,19 +71,13 @@ void ListaIndexada::modificar(int posicion,int dato){
 int ListaIndexada::numElem(){
     return this->contador;
 }
-ListaIndexada::Nodo::Nodo(int dato){
+ListaIndexada::Caja::Caja(int dato){
     this->dato=dato;
     this->siguiente=nullptr;
 }
-ListaIndexada::Nodo::~Nodo(){
+ListaIndexada::Caja::~Caja(){
     if(this->siguiente!=nullptr){
         delete this->siguiente;
     }
-}
-void ListaIndexada::imprimir(){
-    for(int i=1;i<=this->contador;++i){
-        cout << recuperar(i) << " ";
-    }
-    cout <<endl;
 }
 
