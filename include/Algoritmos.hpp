3828be3ea93798a5bda9ce8be469ab36de3d186a
Jean Paul Chacon <jean.chacongonzalez@ucr.ac.cr>
2022-10-27 17:18:33 -0600

    Se agregan todas las opciones del menú en el string que se imprime

--- include/Algoritmos.hpp
+++ include/Algoritmos.hpp
@@ -18,11 +18,6 @@ Algoritmos();
 //TODO: Programar este metodo
 void inicializarArbol();
 void menu();
-
-void hermanoIzquierdo(Arbol::Nodo*);  // Por dentro imprime la etiqueta, no devuelve nada entonces.
-void contieneEtiquetasRepetidas();  // Imprime por dentro el mensaje de verdadero o falso.
-void profundidadNodo(Arbol::Nodo*);  // Imprime por dentro el numero.
-void cantidadNivelesPreOrden();  // Imprime la cantidad de niveles por dentro.
 int contarPorNiveles();
 void listaEtiquetasNivel(int nivel);
 void recorridoPreOrden();
