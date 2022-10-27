1453ba68c521001e093212de099a032e35a91488
Jean Paul Chacon <jean.chacongonzalez@ucr.ac.cr>
2022-10-27 13:21:41 -0600

    Se agrega estructura compatible con Arbol::Nodo* en ArregloPadre

--- include/ArregloPadre.hpp
+++ include/ArregloPadre.hpp
@@ -11,22 +11,22 @@
 
 typedef class ArregloPadre {
 public:
-    struct Nodo {    
-        Nodo(int nodo_id) : nodo(nodo_id) {};
-        int nodo;
-    };
-    struct contenedorArreglo {
+    // struct Nodo {
+    //     Nodo(int nodo_id) : nodo(nodo_id) {};
+    //     int nodo;
+    // };
+    typedef struct contenedorArreglo {
 //        TipoNodo* nodo;
         contenedorArreglo(int et, int padre, int nodo_id) : etiqueta(et), padre(padre), nodo_id(nodo_id) {};
         // contenedorArreglo() {};
         // Nodo nulo es constructor vacio
         // Nodo_id va a tener la pos del arreglo empezando en 0, pero por fuera, cuando manden nodo_id tengo que restarle
         // 1 porque para el usuario, el arreglo va a empezar en 1.
-        Nodo nodo_id;
+        int nodo_id;
         int etiqueta;
         int padre;
-    };
-    
+    } Nodo;
+
 
     std::vector<contenedorArreglo> arreglo;
 
@@ -42,26 +42,26 @@ public:
 
 
 //    void AgregarHijo(int etiqueta, TipoNodo elemento);
-    int AgregarHijo(int nodoPadre, int etiqueta);
+    ArregloPadre::contenedorArreglo* AgregarHijo(contenedorArreglo* nodoPadre,int etiqueta);
 
 
 //    void AgregarHijoMasDerecho(TipoNodo elemento);
-    int AgregarHijoMasDerecho(int nodoPadre, int etiqueta);
+    ArregloPadre::contenedorArreglo* AgregarHijoMasDerecho(contenedorArreglo* nodoPadre, int etiqueta);
 
 //    void BorrarHoja(std::string etiqueta);
-    void BorrarHoja(int nodo_id_externo);
+    void BorrarHoja(ArregloPadre::contenedorArreglo* nodo_id_externo);
 
-    int Raiz();
+    ArregloPadre::contenedorArreglo* Raiz();
 
-    int Padre(int nodo_id_externo);
+    ArregloPadre::contenedorArreglo* Padre(ArregloPadre::contenedorArreglo* nodo_id_externo);
 
-    int HijoMasIzquierdo(int nodo_id_externo);
+    ArregloPadre::contenedorArreglo* HijoMasIzquierdo(ArregloPadre::contenedorArreglo* nodo_id_externo);
 
-    int HermanoDerecho(int nodo_id_hermano);
+    ArregloPadre::contenedorArreglo* HermanoDerecho(ArregloPadre::contenedorArreglo* nodo_id_hermano);
 
-    int Etiqueta(int nodo_id_externo);
+    int Etiqueta(ArregloPadre::contenedorArreglo* nodo_id_externo);
 
-    void ModificaEtiqueta(int etiqueta, int nodo_id_externo);
+    void ModificarEtiqueta(ArregloPadre::contenedorArreglo* nodo_id_externo, int etiqueta);
 
     int NumNodos();
 
